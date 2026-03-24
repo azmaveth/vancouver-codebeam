37a71f190cd261e56cded92881705276538939dd
Nicholas Geraedts <nicholas.geraedts@hiive.com>
2026-03-24 14:23:45 -0700

    add tidewave

--- no_thanks/mix.exs
+++ no_thanks/mix.exs
@@ -40,6 +40,8 @@ defmodule NoThanks.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:igniter, "~> 0.6", only: [:dev, :test]},
+      {:tidewave, "~> 0.5", only: [:dev]},
       {:phoenix, "~> 1.8.5"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},

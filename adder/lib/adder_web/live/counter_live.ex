cdf3bb267e6d472a391d1c1b34ef367562dd58d9
Quintin <12143900+quintinm-dev@users.noreply.github.com>
2026-03-24 14:56:57 -0700

    Extract counter function component

--- adder/lib/adder_web/live/counter_live.ex
+++ adder/lib/adder_web/live/counter_live.ex
@@ -19,22 +19,39 @@ defmodule AdderWeb.CounterLive do
 
   def render(assigns) do
     ~H"""
+    <.counter value={Counter.show(@counter)}>
+      <.counter_controls />
+    </.counter>
+    """
+  end
+
+  attr :value, :string, required: true
+  slot :inner_block, required: true
+
+  def counter(assigns) do
+    ~H"""
     <div class="flex flex-col items-center gap-8 py-16">
-      <h1 class="text-4xl font-bold">{Counter.show(@counter)}</h1>
-      <div class="flex gap-4">
-        <button
-          phx-click="decrement"
-          class="rounded-lg bg-zinc-900 px-6 py-3 text-white hover:bg-zinc-700"
-        >
-          -1
-        </button>
-        <button
-          phx-click="increment"
-          class="rounded-lg bg-zinc-900 px-6 py-3 text-white hover:bg-zinc-700"
-        >
-          +1
-        </button>
-      </div>
+      <h1 class="text-4xl font-bold">{@value}</h1>
+      {render_slot(@inner_block)}
+    </div>
+    """
+  end
+
+  def counter_controls(assigns) do
+    ~H"""
+    <div class="flex gap-4">
+      <button
+        phx-click="decrement"
+        class="rounded-lg bg-zinc-900 px-6 py-3 text-white hover:bg-zinc-700"
+      >
+        -1
+      </button>
+      <button
+        phx-click="increment"
+        class="rounded-lg bg-zinc-900 px-6 py-3 text-white hover:bg-zinc-700"
+      >
+        +1
+      </button>
     </div>
     """
   end
